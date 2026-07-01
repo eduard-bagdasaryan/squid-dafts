0c5afab94c9cbe9ce32653cf2f7822bd9d33af0f
Eduard Bagdasaryan <eduard.bagdasaryan@measurement-factory.com>
2026-07-01 15:01:06 +0300

    Update hit-revalidation.js

--- tests/hit-revalidation.js
+++ tests/hit-revalidation.js
@@ -64,7 +64,7 @@ export default class MyTest extends Test {
         this._workerListeningAddresses = cfg.workerListeningAddresses();
         // TODO: Allow/test collapsed revalidation of non-collapsed hits.
         cfg.collapsedForwarding(Config.sendingOrder() === soTrueCollapsing);
- //       cfg.custom('debug_options ALL,7');
+        cfg.custom('debug_options ALL,5');
     }
 
     static Configurators() {
